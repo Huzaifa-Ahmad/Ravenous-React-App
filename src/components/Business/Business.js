9011a34adc892e93df8bbcc765e52c6f75f1b2e3
Huzaifa Ahmad <huzaifa.ahmad1347@gmail.com>
2019-12-30 18:03:29 -0500

    Final Version

--- src/components/Business/Business.js
+++ src/components/Business/Business.js
@@ -3,8 +3,6 @@ import "./Business.css";
 
 class Business extends React.Component {
   render() {
-    // const { business } = this.props;
-
     return (
       <div className="Business">
         <div className="image-container">
