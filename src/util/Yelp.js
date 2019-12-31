a9ea995ed1016cc35ebc20b34af26b8738baf2b4
Huzaifa Ahmad <huzaifa.ahmad1347@gmail.com>
2019-12-30 20:28:55 -0500

    filter for halal

--- src/util/Yelp.js
+++ src/util/Yelp.js
@@ -7,7 +7,7 @@ const apiKey = process.env.REACT_APP_YELP_API_KEY;
 const Yelp = {
   searchYelp(term, location, sortBy) {
     return fetch(
-      `https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`,
+      `https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}&categories=halal`,
       {
         headers: {
           Authorization: `Bearer ${apiKey}`
