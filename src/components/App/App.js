e4bac97a6c4ee162be2f5ec9da6cf591b4c79a9d
Huzaifa Ahmad <huzaifa.ahmad1347@gmail.com>
2020-01-01 23:03:34 -0500

    Final Commit

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,21 +4,6 @@ import BusinessList from "../BusinessList/BusinessList";
 import SearchBar from "../SearchBar/SearchBar";
 import Yelp from "../../util/Yelp";
 
-// const business = {
-//   imageSrc:
-//     "https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg",
-//   name: "MarginOtto Pizzeria",
-//   address: "1010 Paddington Way",
-//   city: "Bordertown",
-//   state: "NY",
-//   zipCode: "10101",
-//   category: "Italian",
-//   rating: 4.5,
-//   reviewCount: 90
-// };
-
-// const businesses = [business, business, business, business, business, business];
-
 class App extends React.Component {
   constructor(props) {
     super(props);
@@ -36,7 +21,6 @@ class App extends React.Component {
   }
 
   render() {
-    // console.log(typeof this.state.businesses);
     return (
       <div className="App">
         <h1>ravenous</h1>
